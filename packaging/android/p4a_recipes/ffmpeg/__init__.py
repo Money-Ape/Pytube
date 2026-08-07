8cdaf0f5087c4fbc57a634e063d63a8d9f2b21eb
Money-Ape <work.moneyapenetwork@gmail.com>
2026-08-07 14:00:10 +0530

    debug(ffmpeg): add install libs for libffmpegbin

--- packaging/android/p4a_recipes/ffmpeg/__init__.py
+++ packaging/android/p4a_recipes/ffmpeg/__init__.py
@@ -4,9 +4,6 @@ import os, shutil
 class FFmpegRecipe(Recipe):
     version = "1.0"
     url = None
-    built_libraries = {
-        "libffmpegbin.so": "."
-    }
 
     def build_arch(self, arch):
         src = os.path.join(self.recipe_dir, arch.arch, "libffmpegbin.so")
@@ -15,6 +12,13 @@ class FFmpegRecipe(Recipe):
         os.makedirs(build_dir, exist_ok=True)
 
         dst = os.path.join(build_dir, "libffmpegbin.so")
+        shutil.copy2(src, dst)
+
+    def install_libraries(self, arch):
+        build_dir = self.get_build_dir(arch.arch)
+        src = os.path.join(build_dir, "libffmpegbin.so")
+
+        dst = os.path.join(self.ctx.get_libs_dir(arch.arch), "libffmpegbin.so")
         shutil.copy2(src, dst)
 
 recipe = FFmpegRecipe()
